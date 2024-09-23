ad8fc23a8551be89dd18e638c6b1618a502c17cc
unknown <footballwhitestars@gmail.com>
2024-09-23 16:15:10 +0200

    Made filters permanent, removed slider, drivers loaded via API

--- main.py
+++ main.py
@@ -14,6 +14,53 @@ from numpy import cumsum, divide
 
 from src import utils
 
+def get_drivers(data_in):
+    # per driver (id is number), returns a dict with listed data
+    drivers = {}
+    for driver_item in data_in:
+        drivers[driver_item['driver_number']] = {
+            'driver_number': driver_item['driver_number'],
+            'country_code': driver_item['country_code'],
+            'first_name': driver_item['first_name'],
+            'headshot_url': driver_item['headshot_url'],
+            'last_name': driver_item['last_name'],
+            'team_colour': '#' + driver_item['team_colour'].lower(),
+            'team_name': driver_item['team_name'],
+            'name_acronym': driver_item['name_acronym']
+        }
+    return drivers
+
+with open('mock_data/drivers_baku.json') as json_file:
+    mock_drivers_data = json.load(json_file)
+drivers = get_drivers(mock_drivers_data)
+try:
+    response = requests.get(f'https://api.openf1.org/v1/drivers?session_key=latest')
+    if response.status_code == 200:
+        if len(response.json()) == 0:
+            print(f"[{str(datetime.now())}] Server response empty")
+        else:
+            drivers = get_drivers(response.json())
+    else:
+        print(f"[{str(datetime.now())}] Server response: {response.status_code}")
+except Exception as e:
+    print(f"[{str(datetime.now())}] Error retrieving data: {e}")
+
+# Opening JSON files with mock data
+with open('mock_data/baku.json') as json_file:
+    mock_data = json.load(json_file)
+data = mock_data #TODO: data should still be initialized
+
+# Prepare the traces for the line plot
+traces = []
+for driver in drivers.values():
+    traces.append(go.Scatter(
+        x = [0],  # X-axis: lap numbers
+        y = [0], # Y-axis: cumulated lap times
+        mode = 'lines+markers',
+        name = driver['name_acronym'],
+        line_color = driver['team_colour']
+    ))
+
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
@@ -23,6 +70,7 @@ app.layout = html.Div([
     html.Div([
         dcc.Graph(id = 'live-lap-time-graph',
                   figure= go.Figure(
+                        data = traces,
                         layout = go.Layout(
                             title = 'Live Lap Times',
                             xaxis = {'title': 'Lap Number'},
@@ -34,31 +82,9 @@ app.layout = html.Div([
             id = 'interval-component',
             interval = 10000,  # Update every x milliseconds
             n_intervals = 0
-        ),
-        dcc.Slider(
-            1, #@TODO: change all these values (based on race laps?)
-            80,
-            step = None,
-            value = 3,
-            id = 'lap-slider'
         )
     ])
 ])
-
-def get_drivers(data_in):
-    # per driver (id is number), returns a dict with listed data
-    drivers = {}
-    for driver_item in data_in:
-        drivers[driver_item['driver_number']] = {
-            'country_code': driver_item['country_code'],
-            'first_name': driver_item['first_name'],
-            'headshot_url': driver_item['headshot_url'],
-            'last_name': driver_item['last_name'],
-            'team_colour': '#' + driver_item['team_colour'].lower(),
-            'team_name': driver_item['team_name'],
-            'name_acronym': driver_item['name_acronym']
-        }
-    return drivers
 
 def get_driver_laps(data_in):
     #per driver (id is number), returns a dict with lap: lap time
@@ -155,32 +181,21 @@ def get_driver_diff_std_laps(data_in, std_lap_time):
             driver_laps[driver][lap] = driver_laps[driver][lap] - std_lap_time
     return driver_laps
 
-# Opening JSON files with mock data
-with open('mock_data/baku.json') as json_file:
-    mock_data = json.load(json_file)
-with open('mock_data/drivers_singapore.json') as json_file:
-    mock_drivers_data = json.load(json_file)
-data = mock_data #TODO: data should still be initialized
-drivers = get_drivers(mock_drivers_data)
-
 # Callback to update the graph
 @app.callback(Output('live-lap-time-graph', 'figure'),
-    Output('lap-slider', 'value'),
-    Input('interval-component', 'n_intervals'),
-    Input('lap-slider', 'value'),
-    Input('live-lap-time-graph', 'figure')
+              Input('interval-component', 'n_intervals'),
+              Input('live-lap-time-graph', 'figure')
 )
-def update_graph_live(n, counter, fig):
-    data1 = []
+def update_graph_live(n, fig):
+    live_data = []
     #Perform GET request to get the latest data from the local server
     try:
         response = requests.get(f'https://api.openf1.org/v1/laps?session_key=latest')
-        #response = requests.get(f'https://api.openf1.org/v1/laps?session_key=latest&lap_number<{counter}')
         if response.status_code == 200:
             if len(response.json()) == 0:
                 print(f"[{str(datetime.now())}] Server response empty")
             else:
-                data1 = response.json()
+                live_data = response.json()
                 #print(data)
         else:
             #data = {}
@@ -189,74 +204,20 @@ def update_graph_live(n, counter, fig):
         print(f"[{str(datetime.now())}] Error retrieving data: {e}")
         #data = {}
 
+    out_figure = fig
+    if len(live_data) > 0:
+        laps_data = get_driver_diff_median_laps(live_data)
 
-    if len(data1) > 0:
-        #counter += 1
-        # laps_data = get_driver_laps(data)
-        # print(laps_data)
-        #
-        # laps_avg_data = get_average_laps(data)
-        # print(laps_avg_data)
-
-        #laps_data = get_driver_diff_avg_laps(data)
-        #laps_data = get_driver_diff_moving_avg_laps(data)
-        #laps_data = get_driver_diff_std_laps(data, 95)
-        laps_data = get_driver_diff_median_laps(data1)
-
-        # Prepare the traces for the line plot
-        traces = []
+        # Update the traces for the line plot
+        out_figure = go.Figure(fig)
         for driver, lap_times in laps_data.items():
-            #print(f"Driver: {driver} Lap times: {lap_times}")
-            traces.append(go.Scatter(
-                x = list(lap_times.keys()),  # X-axis: lap numbers
-                #y = list(lap_times.values()),  # Y-axis: lap times
-                y = cumsum(list(lap_times.values())), # Y-axis: cumulated lap times
-                mode = 'lines+markers',
-                name = drivers[driver]['name_acronym'],
-                line_color = drivers[driver]['team_colour']
-            ))
+            out_figure.update_traces(dict(x=list(lap_times.keys()),
+                                          y=cumsum(list(lap_times.values()))),
+                                     selector=({'name':drivers[driver]['name_acronym']}))
 
-        # Create the figure
-        # fig = go.Figure(
-        #     data = traces,
-        #     layout = go.Layout(
-        #         title = 'Live Lap Times',
-        #         xaxis = {'title': 'Lap Number'},
-        #         yaxis = {'title': 'Lap Time (seconds)', 'autorange': 'reversed'},
-        #         hovermode = 'closest',
-        #         height = 1000
-        #     )
-        # )
-        #print(fig)
-        fig['data'] = []
-        for trace in traces:
-            fig['data'].append(trace)
-
-    return fig, counter
+    return out_figure
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
     #app.run_server(host = '0.0.0.0', debug = False)
-
-    # with open('mock_data/canada.json') as json_file:
-    #     mock_data = json.load(json_file)
-    # with open('mock_data/drivers_canada.json') as json_file:
-    #     mock_drivers_data = json.load(json_file)
-    # data = mock_data
-    # drivers = get_drivers(mock_drivers_data)
-
-    #avg = get_average_laps(data)
-    #avg = get_moving_average_laps(data)
-    #avg = get_median_laps(data)
-    #laps_data = get_driver_diff_avg_laps(data)
-    # laps_data = get_driver_diff_moving_avg_laps(data)
-    # laps_data = get_driver_diff_std_laps(data, 110)
-    #print(avg)
-    #print(laps_data)
-
-    #time1 = datetime("2024-06-09T18:23:21.540000+00:00")
-    # time1 = parser.isoparse("2024-06-09T18:23:21.540000+00:00")
-    # print(time1.strftime("%H:%M:%S.%f"))
-    # time2 = float(time1.strftime("%M")) * 60 + float(time1.strftime("%S.%f"))
-    # print(time2)
