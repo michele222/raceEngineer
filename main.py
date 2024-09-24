ffe1444d63021cdec85a4e117364f65b202ae3e6
unknown <footballwhitestars@gmail.com>
2024-09-24 17:01:02 +0200

    Live update controls, theme

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from datetime import datetime
-from pickle import EMPTY_SET, EMPTY_LIST
 from statistics import median
 from dateutil import parser
 
 import dash
 import requests
 import json
-import time
 import plotly.graph_objs as go
 from dash import dcc, html
-from dash.dependencies import Output, Input
-from numpy import cumsum, divide
+from dash.dependencies import Output, Input, State
+import dash_bootstrap_components as dbc
+from numpy import cumsum
 
 from src import utils
+
+interval = 30 #update every x seconds
 
 def get_drivers(data_in):
     # per driver (id is number), returns a dict with listed data
@@ -45,6 +46,19 @@ try:
 except Exception as e:
     print(f"[{str(datetime.now())}] Error retrieving data: {e}")
 
+event_title = 'Grand Prix'
+try:
+    response = requests.get(f'https://api.openf1.org/v1/meetings?meeting_key=latest')
+    if response.status_code == 200:
+        if len(response.json()) == 0:
+            print(f"[{str(datetime.now())}] Server response empty")
+        else:
+            event_title = response.json()[0]['meeting_official_name']
+    else:
+        print(f"[{str(datetime.now())}] Server response: {response.status_code}")
+except Exception as e:
+    print(f"[{str(datetime.now())}] Error retrieving data: {e}")
+
 # Opening JSON files with mock data
 with open('mock_data/baku.json') as json_file:
     mock_data = json.load(json_file)
@@ -62,29 +76,79 @@ for driver in drivers.values():
     ))
 
 # Initialize the Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])#, update_title=None)
+
+tab1_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.Div([
+                dcc.Graph(id='live-lap-time-graph',
+                          figure=go.Figure(
+                              data=traces,
+                              layout=go.Layout(
+                                  title=f'{event_title} - Race Trace',
+                                  xaxis={'title': 'Lap Number'},
+                                  yaxis={'title': 'Gap (seconds)', 'autorange': 'reversed'},
+                                  hovermode='closest',
+                                  height=1000
+                              ))),
+                dcc.Interval(
+                    id='interval-component',
+                    interval=interval * 1000,  # Update every x milliseconds
+                    n_intervals=0,
+                    disabled=False
+                ),
+                html.Small(id = "last-update-text", className = "text-muted"),
+                dbc.Checkbox(
+                    id="live-update-checkbox",
+                    label="Live updates",
+                    value=True,
+                ),
+                dbc.Fade(
+                    dbc.Card(
+                        dbc.CardBody([
+                            html.Label("Update interval (seconds)"),
+                            dbc.Select(list(range(5, 60, 5)),
+                                       interval,
+                                       id="interval-select",
+                                       )
+                        ]),
+                        style={"width": "18rem"},
+                    ),
+                    id="live-update-fade",
+                    is_in=True,
+                    appear=True,
+                ),
+
+            ])
+        ]
+    ),
+    className="mt-3",
+)
+
+tab2_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.P("Nothing to see here yet", className="card-text"),
+            dbc.Button("Do nothing", color="danger"),
+        ]
+    ),
+    className="mt-3",
+)
+
+tabs = dbc.Tabs(
+    [
+        dbc.Tab(tab1_content, label="Race Trace"),
+        dbc.Tab(tab2_content, label="Live Gaps"),
+    ]
+)
 
 # Layout of the app
 app.layout = html.Div([
-    html.H1("Live Lap Time Data"),
-    html.Div([
-        dcc.Graph(id = 'live-lap-time-graph',
-                  figure= go.Figure(
-                        data = traces,
-                        layout = go.Layout(
-                            title = 'Live Lap Times',
-                            xaxis = {'title': 'Lap Number'},
-                            yaxis = {'title': 'Lap Time (seconds)', 'autorange': 'reversed'},
-                            hovermode = 'closest',
-                            height = 1000
-                        ))),
-        dcc.Interval(
-            id = 'interval-component',
-            interval = 10000,  # Update every x milliseconds
-            n_intervals = 0
-        )
-    ])
-])
+    html.H1("raceEngineer"),
+    tabs
+], className="m-4")
+
 
 def get_driver_laps(data_in):
     #per driver (id is number), returns a dict with lap: lap time
@@ -95,6 +159,7 @@ def get_driver_laps(data_in):
             if lap_item['lap_number'] == 2:
                 lap_start_iso_time = parser.isoparse(lap_item['date_start'])
                 driver_laps[lap_item['driver_number']] = {
+                    0: 0.0,
                     1: float(lap_start_iso_time.strftime("%M")) * 60 + float(lap_start_iso_time.strftime("%S.%f"))
                 }
         if utils.is_float(lap_item['lap_duration']):
@@ -121,6 +186,7 @@ def get_median_laps(data_in):
             if lap_item['lap_number'] not in avg_laps:
                 avg_laps[lap_item['lap_number']] = []
             if lap_item['lap_number'] == 2:
+                avg_laps[0] = [0.0]
                 if 1 not in avg_laps:
                     avg_laps[1] = []
                 lap_start_iso_time = parser.isoparse(lap_item['date_start'])
@@ -183,10 +249,14 @@ def get_driver_diff_std_laps(data_in, std_lap_time):
 
 # Callback to update the graph
 @app.callback(Output('live-lap-time-graph', 'figure'),
+              Output('last-update-text', 'children'),
               Input('interval-component', 'n_intervals'),
-              Input('live-lap-time-graph', 'figure')
+              State('live-lap-time-graph', 'figure'),
+              State('last-update-text', 'children'),
 )
-def update_graph_live(n, fig):
+def update_graph_live(n, fig, last_update_text):
+    out_figure = fig
+    out_text = last_update_text
     live_data = []
     #Perform GET request to get the latest data from the local server
     try:
@@ -198,16 +268,13 @@ def update_graph_live(n, fig):
                 live_data = response.json()
                 #print(data)
         else:
-            #data = {}
             print(f"[{str(datetime.now())}] Server response: {response.status_code}")
     except Exception as e:
         print(f"[{str(datetime.now())}] Error retrieving data: {e}")
-        #data = {}
 
-    out_figure = fig
     if len(live_data) > 0:
         laps_data = get_driver_diff_median_laps(live_data)
-
+        out_text = f"Last updated on {str(datetime.now())}"
         # Update the traces for the line plot
         out_figure = go.Figure(fig)
         for driver, lap_times in laps_data.items():
@@ -215,7 +282,22 @@ def update_graph_live(n, fig):
                                           y=cumsum(list(lap_times.values()))),
                                      selector=({'name':drivers[driver]['name_acronym']}))
 
-    return out_figure
+    return out_figure, out_text
+
+@app.callback(
+    Output("live-update-fade", "is_in"),
+    Output('interval-component', 'disabled'),
+    Input("live-update-checkbox", "value"),
+)
+def toggle_fade(live_update_checked):
+    return live_update_checked, not live_update_checked
+
+@app.callback(
+    Output('interval-component', 'interval'),
+    Input("interval-select", "value"),
+)
+def change_live_interval(interval):
+    return int(interval)*1000
 
 # Run the Dash app
 if __name__ == '__main__':
